4033ec1fdae94f280aadb257cd6b261b82ebbe1c
FATx64 <73746785+FATx64@users.noreply.github.com>
2021-06-30 08:53:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-import pandas as pd 
+#SKlearn SVM untuk Klasifikasi
 
-#read the data and change to dataframe
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+ 
+iris = datasets.load_iris()
+atribut = iris.data
+label = iris.target
 
-df = pd.read_csv('Social_Network_Ads.csv')
+#bagi data set menjadi train set dan test set
+x_train, x_test, y_train, y_test = train_test_split (atribut, label, test_size = 0.2)
 
-df.head()
+#uji akurasi tanpa menggunakan PCA
+from sklearn import tree
+ 
+decision_tree = tree.DecisionTreeClassifier()
+model_pertama = decision_tree.fit(X_train, y_train)
+model_pertama.score(X_test, y_test)
+#output : 0.36666666666666664
+
+from sklearn.decomposition import PCA
+ 
+# membuat objek PCA dengan 4 principal component
+pca = PCA(n_components=4)
+ 
+# mengaplikasikan PCA pada dataset
+pca_attributes = pca.fit_transform(X_train)
+ 
+# melihat variance dari setiap atribut
+pca.explained_variance_ratio_
+#output : array([0.92663144, 0.05028605, 0.01734791, 0.0057346 ])
+
+# PCA dengan 2 principal component
+pca = PCA(n_components = 2)
+X_train_pca = pca.fit_transform(X_train)
+X_test_pca = pca.fit_transform(X_test)
+
+# uji akurasi classifier
+model2 = decision_tree.fit(X_train_pca, y_train)
+model2.score(X_test_pca, y_test)
+#output : 0.16666666666666666
